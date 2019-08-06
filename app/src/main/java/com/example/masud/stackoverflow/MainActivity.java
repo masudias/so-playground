81087be8ba516e7c941b42a85edd0e8572c63bf3
Reaz Murshed <masud@wc-dhcp247d240.employee-secure.wireless.fsu.edu>
2019-08-06 14:15:39 -0400

    Stackoverflow Question - https://stackoverflow.com/questions/57379774/how-to-align-the-options-menu

--- app/src/main/java/com/example/masud/stackoverflow/MainActivity.java
+++ app/src/main/java/com/example/masud/stackoverflow/MainActivity.java
@@ -2,12 +2,77 @@ package com.example.masud.stackoverflow;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.PopupMenu;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.lang.reflect.Field;
+
+public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = findViewById(R.id.menu_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menu_items:
+                showPopupMenu();
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    public void showPopupMenu() {
+        PopupMenu popup = new PopupMenu(this, findViewById(R.id.menu_items));
+        popup.setOnMenuItemClickListener(this);
+        popup.inflate(R.menu.popup_menu);
+        popup.setGravity(Gravity.END);
+
+        Object menuHelper;
+        Class[] argTypes;
+        try {
+            Field fMenuHelper = PopupMenu.class.getDeclaredField("mPopup");
+            fMenuHelper.setAccessible(true);
+            menuHelper = fMenuHelper.get(popup);
+            argTypes = new Class[]{boolean.class};
+            menuHelper.getClass().getDeclaredMethod("setForceShowIcon", argTypes).invoke(menuHelper, true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        popup.show();
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.invite:
+                Toast.makeText(this, "Invite", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.download:
+                Toast.makeText(this, "Download", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return false;
+        }
     }
 }
